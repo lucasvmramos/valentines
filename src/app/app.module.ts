76cc9fc562a22d5e12925abc2860e6b323ec515c
Lucas Vinicius <lucasvmr6@outlook.com>
2023-05-20 19:29:06 -0300

    commit inicial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,16 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { CabecalhoComponent } from './componente/cabecalho/cabecalho.component';
+import { ConteudoComponent } from './componente/conteudo/conteudo.component';
+import { RodapeComponent } from './componente/rodape/rodape.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    CabecalhoComponent,
+    ConteudoComponent,
+    RodapeComponent
   ],
   imports: [
     BrowserModule,
